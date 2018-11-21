fdbf16e183ab38af8dc03690b5880308f3af4bd4
Martin Atsinov <martin.atsinov@isobar.com>
2018-11-21 17:33:58 +0200

    minor updates

--- src/beacnd.js
+++ src/beacnd.js
@@ -13,7 +13,8 @@ interface Mark {
 }
 
 class Beacnd {
-  url: string
+  token: string = 'D9BBFC8D-3883-4743-800C-2F2716F1205B';
+  url: string = 'https://splunk.ecommera.com/services/collector/event'
   send (type: string, data: {}) {
     if (navigator.sendBeacon && this.url) {
       const payload = new Blob([JSON.stringify(data)], { type: 'application/json' })
